c67a0f4ee6c3895ab5ca5850771e7ef800fdbbce
Camilo <camijimemo@gmail.com>
2024-09-09 22:37:39 -0300

    agregada la clase jugadores, cambie cartas un poco

---
+++ src/main/java/edu/uca/Cartas.java
@@ -0,0 +1,14 @@
+package edu.uca;
+
+public class Cartas {
+    String tribu;
+    String region;
+    String color;
+
+    public Cartas(String tribu, String region, String color) {
+        this.tribu = tribu;
+        this.region = region;
+        this.color = color;
+    }
+
+}
